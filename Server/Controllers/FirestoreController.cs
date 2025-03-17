c68dbdb71e30295d94c4048f06fe4d159bd53905
Gray-Barrantes <barrantesg87@gmail.com>
2025-03-17 17:30:33 -0600

    .

---
+++ Server/Controllers/FirestoreController.cs
@@ -0,0 +1,30 @@
+﻿using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gerencia.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FirestoreController : ControllerBase
+    {
+        private readonly FirestoreDb _firestoreDb;
+
+        public FirestoreController(FirestoreDb firestoreDb)
+        {
+            _firestoreDb = firestoreDb;
+        }
+
+        [HttpGet("users")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = new List<object>();
+            var snapshot = await _firestoreDb.Collection("users").GetSnapshotAsync();
+
+            foreach (var doc in snapshot.Documents)
+            {
+                users.Add(doc.ToDictionary());
+            }
+            return Ok(users);
+        }
+    }
+}
